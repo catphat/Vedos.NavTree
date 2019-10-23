d32b09c64ba2b36ca8a18db1df424206d6012473
Kian <kian@vedos.io>
2019-10-22 22:33:44 -0600

    add BreadthFirstTraversal/Descendents extensions method and associated test

---
+++ src/Vedos.NavTree.Models/NavTreeNodeHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Vedos.NavTree.Models
+{
+    public static class NavTreeNodeHelper
+    {
+        
+       
+        public static IEnumerable<NavTreeNode> BreadthFirstTraversal(this NavTreeNode root)
+        {
+            var nodes = new Queue<NavTreeNode>(new []{root});
+            while (nodes.Any())
+            {
+                var node = nodes.Dequeue();
+                yield return node;
+                foreach (var cn in node.Children()) nodes.Enqueue(cn);
+            }
+        }
+
+
+        public static IEnumerable<NavTreeNode> Descendents(this NavTreeNode root)
+        {
+            return root.BreadthFirstTraversal().Where(x => x.Id != root.Id);
+        }
+ 
+    }
+}
--- tests/Vedos.NavTree.Tests/NavTreeModelTests.cs
+++ tests/Vedos.NavTree.Tests/NavTreeModelTests.cs
@@ -35,49 +35,90 @@ namespace Vedos.NavTree.Tests
                     }
                 }
             };
-            
+
             Assert.True(navTree.Any());
             Assert.Null(navTree.Parent);
             Assert.Equal(2, navTree.Children().Count());
             Assert.Equal(3, navTree.Children().First(x => x.Id == "root-child-a").Children().Count());
             Assert.Equal(3, navTree.Children().First(x => x.Id == "root-child-b").Children().Count());
-            Assert.Single(navTree.Children().First(x => x.Id == "root-child-b").Children().First(x => x.Id == "2-child-of-b"));
+            Assert.Single(navTree.Children().First(x => x.Id == "root-child-b").Children()
+                .First(x => x.Id == "2-child-of-b"));
         }
 
         [Fact]
         public void CanLocateChildNodeById()
         {
-             var navTree = new NavTreeNode("root")
-             {
-                 new NavTreeNode("root-child-a")
-                 {
-                     new NavTreeNode("0-child-of-a"),
-                     new NavTreeNode("1-child-of-a"),
-                     new NavTreeNode("2-child-of-a"),
-                 },
-                 new NavTreeNode("root-child-b")
-                 {
-                     new NavTreeNode("0-child-of-b"),
-                     new NavTreeNode("1-child-of-b"),
-                     new NavTreeNode("2-child-of-b")
-                     {
-                         new NavTreeNode("0-child-of-2b")
-                     }
-                 }
-             };
+            var navTree = new NavTreeNode("root")
+            {
+                new NavTreeNode("root-child-a")
+                {
+                    new NavTreeNode("0-child-of-a"),
+                    new NavTreeNode("1-child-of-a"),
+                    new NavTreeNode("2-child-of-a"),
+                },
+                new NavTreeNode("root-child-b")
+                {
+                    new NavTreeNode("0-child-of-b"),
+                    new NavTreeNode("1-child-of-b"),
+                    new NavTreeNode("2-child-of-b")
+                    {
+                        new NavTreeNode("0-child-of-2b")
+                    }
+                }
+            };
 
 
-             var firstChild = navTree.GetChild("root-child-b");
-             Assert.NotNull(firstChild);
-             Assert.Equal("root", firstChild.Parent.Id);
+            var firstChild = navTree.GetChild("root-child-b");
+            Assert.NotNull(firstChild);
+            Assert.Equal("root", firstChild.Parent.Id);
 
-             var secondChild = firstChild.GetChild("2-child-of-b");
-             Assert.NotNull(secondChild);
-             Assert.Equal("root-child-b", secondChild.Parent.Id);
+            var secondChild = firstChild.GetChild("2-child-of-b");
+            Assert.NotNull(secondChild);
+            Assert.Equal("root-child-b", secondChild.Parent.Id);
 
-             var thirdChild = secondChild.GetChild("0-child-of-2b");
-             Assert.NotNull(thirdChild);
-             Assert.Equal("2-child-of-b", thirdChild.Parent.Id);
+            var thirdChild = secondChild.GetChild("0-child-of-2b");
+            Assert.NotNull(thirdChild);
+            Assert.Equal("2-child-of-b", thirdChild.Parent.Id);
+        }
+
+
+        [Fact]
+        public void CanGetNodeDescendentsViaDescendentsStaticExtensionMethod()
+        {
+            var navTree = new NavTreeNode("root")
+            {
+                new NavTreeNode("root-child-a")
+                {
+                    new NavTreeNode("0-child-of-a"),
+                    new NavTreeNode("1-child-of-a"),
+                    new NavTreeNode("2-child-of-a"),
+                },
+                new NavTreeNode("root-child-b")
+                {
+                    new NavTreeNode("0-child-of-b"),
+                    new NavTreeNode("1-child-of-b"),
+                    new NavTreeNode("2-child-of-b")
+                    {
+                        new NavTreeNode("0-child-of-2b")
+                    }
+                }
+            };
+
+            var firstChild = navTree.GetChild("root-child-a");
+            Assert.NotNull(firstChild);
+            
+            var firstChildDescendents = firstChild.Descendents();
+            Assert.Equal(3, firstChildDescendents.Count());
+ 
+            var secondChild = navTree.GetChild("root-child-b");
+            Assert.NotNull(secondChild);
+            
+            var secondChildDescendents = secondChild.Descendents();
+            Assert.Equal(4, secondChildDescendents.Count());
+
+            var lastChild = navTree.Descendents().Where(x => x.Id == "0-child-of-2b");
+            Assert.NotNull(lastChild);
+
         }
     }
 }
